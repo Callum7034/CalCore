de047d3a57606a229ff3a2dd6ac7019db973ffc9
Callum7034 <crutledge2015@gmail.com>
2023-04-26 23:21:30 +1200

    Added colour handler for lore (List<String>)

--- src/main/java/com/rcallum/CalCore/Utils/Colour.java
+++ src/main/java/com/rcallum/CalCore/Utils/Colour.java
@@ -2,8 +2,18 @@ package com.rcallum.CalCore.Utils;
 
 import org.bukkit.ChatColor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Colour {
     public static String c(String input) {
         return ChatColor.translateAlternateColorCodes('&', input);
+    }
+    public static List<String> c(List<String> in) {
+        List<String> out = new ArrayList<>();
+        for (String line : in) {
+            out.add(c(line));
+        }
+        return out;
     }
 }
